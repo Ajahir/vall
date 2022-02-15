d47ca7d10d44414ca8cce0fc1ff2b83b1fa65268
aeris.ahir <aeris.ahir@1R-75>
2022-02-15 17:50:11 +0530

    Add project files.

---
+++ vall/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 employee = new Class1
+            {
+                Name = "aeris",
+                Age = 25,
+                UserName = "00xtz",
+                Email = "xyz@",
+                Password = "12376558",
+                ConfirmPassword = "12345678",
+                PhoneNumber = "(123)-(765)-(9085)",
+
+
+            };
+
+            Class2 tester = new Class2();
+            tester.Test(employee);
+        }
+    }
+}
